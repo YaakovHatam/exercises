47e3fa66a239aa5cd90ffcdc1174c3f4f68c090f
Yaakov Hatam <6986221+YaakovHatam@users.noreply.github.com>
2022-06-06 20:02:18 +0300

    airlines exrcise

---
+++ 09-airlines-company/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
